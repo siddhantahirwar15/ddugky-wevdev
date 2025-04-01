2046d5e1cef05a5de0f9c3bbf07b0efd71d720ec
Siddhant Ahirwar <siddhantahirwar@gmail.comgit config user.gmailgit config --global user.email siddhantahirwar@gmail.com>
2025-04-01 11:09:22 +0530

    data types and variables

---
+++ JS-Fundamentals/index.js
@@ -0,0 +1,29 @@
+//////////////////////
+// 01. DATA TYPES
+//////////////////////////////
+
+//1. string ,"Siddhant ahirwar " "20" "true"
+//2. Number ,20
+//3. BigInt
+//4. Boolean ,true/false
+//5. Undefined
+//6. null
+//7. symbol
+
+/////////////////////////////////////
+// 02. variables
+///////////////////////////////////////
+
+//Indenifirs/Name & Literals/valum
+var firstName ="Siddhant";
+var lastName ="ahirawr";
+var age = 20
+var married =false;
+var wafeName // undefined
+var thirdName = null;
+
+console.log(firstName,lastName)
+//print :I"good mornig siddhant ahirwar!"
+console.log("good mornig",firstName,lastName,"!")
+console.log("hi, my name is",firstName,"and my age is",age,".");
+
