8ab6f995bfa1d226e2ca22a467d34538f9e3f308
Siddhant Ahirwar <siddhantahirwar@gmail.comgit config user.gmailgit config --global user.email siddhantahirwar@gmail.com>
2025-04-02 11:51:06 +0530

    operation

--- JS-Fundamentals/02 _operators.js
+++ JS-Fundamentals/02 _operators.js
@@ -1,9 +1,80 @@
-var num1 = 12;
-var num2 = 2; 
-var res1 = num1 + num2; // 14
-var res2 = num1 - num2; // 10
-var res3 = num1 * num2; // 24
-var res4 = num1 / num2; // 6
-var res5 = num1 % num2; // 0
+// var num1 = 12;
+// var num2 = 2;
+// var res1 = num1 + num2; // 14
+// var res2 = num1 - num2; // 10
+// var res3 = num1 * num2; // 24
+// var res4 = num1 / num2; // 6
+// var res5 = num1 % num2; // 0
 
-console.log(res1,res2,res3,res4,res5);
+// console.log(res1, res2, res3, res4, res5);
+
+///////////////////////////////////////////
+// Increment ( ++ ) & Decreemnt ( -- )
+///////////////////////////////////////////
+
+// var a = 4;
+// a = a + 1; // a = 5
+// a++; // a = 6
+// a--; // a = 5
+// a = a - 1; // a = 4
+// a++;
+// a++; // a = 6
+// a++;
+// a--; // a = 6
+
+///////////////////////////////////////////
+// Exponential Operatior (**)
+///////////////////////////////////////////
+// var b = 5;
+// var res1 = b ** 2; // b * b // 25
+// var res2 = b ** 3; // b * b * b // 125
+// console.log(res1, res2); // 25 125
+
+///////////////////////////////////////////
+// Assignment Operators
+// = ( Assignment Oprator)
+// += ( Addition Assignment Operator)
+// -= ( Substraction Assigment Operator )
+///////////////////////////////////////////
+
+// var a = 6;
+// Assign the value "6" into the variable "a"
+// a variable main 6 ko daal do.
+// a += 9; // 15
+// a = a + 9;
+// console.log(a);
+
+// a -= 5;
+// // a = a - 5;
+// console.log(a); // a = 10
+
+// a *= 2;
+// // a = a * 2;
+// console.log(a); // a = 20
+
+// a /= 4;
+// // a = a / 4;
+// console.log(a); // a = 5
+
+// a **= 3;
+// // a = a ** 3;
+// console.log(a); // a = 125
+
+///////////////////////////////////////////
+// COMPARISON OPERATOR
+///////////////////////////////////////////
+var a = 15;
+var b = 10;
+var c = 10;
+console.log(a > b); // true
+console.log(a < b); // false
+console.log(a == b); // false
+console.log(b == c); // true
+
+var d = 5;
+var e = 4;
+console.log(d == e); // false
+console.log(d > e); // true
+console.log(d >= e); // true
+console.log(d <= e); // false
+console.log(d != e); // true
