6879e5eaf351c0b4346d77d2d5b5c1bb85004587
Siddhant Ahirwar <siddhantahirwar@gmail.comgit config user.gmailgit config --global user.email siddhantahirwar@gmail.com>
2025-04-02 11:42:02 +0530

    operators

---
+++ JS-Fundamentals/02 _operators.js
@@ -0,0 +1,9 @@
+var num1 = 12;
+var num2 = 2; 
+var res1 = num1 + num2; // 14
+var res2 = num1 - num2; // 10
+var res3 = num1 * num2; // 24
+var res4 = num1 / num2; // 6
+var res5 = num1 % num2; // 0
+
+console.log(res1,res2,res3,res4,res5);
